7a39ed5986c56e059eea392782081a7271039e5c
yumoqing <yumoqing@gmail.com>
2022-02-24 21:55:20 +0800

    bugfix

--- appPublic/uniqueID.py
+++ appPublic/uniqueID.py
@@ -1,20 +1,9 @@
 import uuid
-
-node = None
+from nanoid import generate
 
 def setNode(n='ff001122334455'):
-	global node
-	if len(n)>12:
-		return
-	for c in n:
-		if c not in ['0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f']:
-			return
-	node = eval('0x' + n + 'L')
+	pass
 
-def getID():
-	global node
-	if node is None:
-		node = uuid.getnode()
-	u = uuid.uuid1(node)
-	return u.hex
+def getID(size=21):
+	return generate(size=size)
 
