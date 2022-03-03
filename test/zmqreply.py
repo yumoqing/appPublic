4a4ef31418caa66e2e18b682b5154abd003a9c93
yumoqing <yumoqing@gmail.com>
2022-03-03 12:52:18 +0800

    bugfix

--- test/zmqreply.py
+++ test/zmqreply.py
@@ -5,14 +5,11 @@ def handler(b):
 	print(b.decode('utf-8'))
 	return 'got it'
 
-x = ZmqReplier('tcp://127.0.0.1:9999', handler)
+x = ZmqReplier('tcp://127.0.0.1:5555', handler)
 x.run()
 f = 0
 while 1:
 	time.sleep(0.1)
-	if f==0:
-		print(dir(x.sock.get_peer()))
-	f += 1
 
 x.stop()
 
