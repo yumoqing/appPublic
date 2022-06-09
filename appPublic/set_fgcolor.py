69577601d37e089fe5f8906b1b83f2a5099e1d18
yumoqing <yumoqing@gmail.com>
2022-06-09 12:45:06 +0800

    bugfix

--- appPublic/set_fgcolor.py
+++ appPublic/set_fgcolor.py
@@ -1,14 +1,12 @@
-# 
+# -*- coding=utf-8 -*-
 
 """
 kivy color:
 [ r, g, b, a]
-
 不同的颜色值总能找到一个人眼感知的灰度值，这是著名的心理学公式：
 灰度 = 红×0.299 + 绿×0.587 + 蓝×0.114
-"""
 当灰度值大于0.5时使用暗色，否则使用明色
-“”“
+"""
 
 def get_fgcolor_from_bgcolor(bgcolor):
 	dark_fgcolor=[0.11,0.11,0.11,1]
@@ -20,4 +18,3 @@ def get_fgcolor_from_bgcolor(bgcolor):
 		return dark_fgcolor
 	else:
 		return bright_fgcolor
-
