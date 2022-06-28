c923c1670dae1e3ce4bd2d01b5dc512123e83bb4
yumoqing <yumoqing@gmail.com>
2022-06-28 20:31:47 +0800

    bugfix

--- appPublic/version.py
+++ appPublic/version.py
@@ -4,3 +4,5 @@
 # __version__ = '5.1.13'
 # replace cryptography with rsa module, because ONLY using rsa modulde in cryptography and cryptography not easy to compile for ios
 __version__ = '5.1.14'
+# keep 5.1.14 version's change
+__version__ = '5.1.15'
